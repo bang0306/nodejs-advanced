b3e31e53699bdf8b0ebaf13e0ba1cbaec8bc8faf
Demon Monk <antdude@126.com>
2018-05-30 22:03:09 +0800

    use worker threads

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const crypto = require('crypto');
 const app = express();
+const Worker = require('webworker-threads').Worker;
 
 app.get('/', (req, res) => {
-    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-        res.send('Hi there!')  
+    const worker = new Worker(function () {
+        // app向worker发消息时，这个函数会被调用
+        this.onmessage = function () {
+            let counter = 0;
+            while (counter < 1e9) {
+                counter ++;
+            }
+            // worker向app发消息
+            postMessage(counter);
+        }
     })
+
+    // 当worker向app发消息时，这个函数会被调用
+    worker.onmessage = function (msg) {
+        console.log(msg.data);
+        res.send(msg.data + '');
+    }
+
+    // app向worker发消息
+    worker.postMessage();
 })
 
 app.get('/fast', (req, res) => {
